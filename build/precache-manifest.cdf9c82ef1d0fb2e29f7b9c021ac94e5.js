a0090fb60709e39c4ed2d47fa74961448a972a2d
vudung45 <vudung45@yahoo.com>
2020-02-21 14:07:14 -0600

    upload build for heruku

---
+++ build/precache-manifest.cdf9c82ef1d0fb2e29f7b9c021ac94e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e3b22080059967e28a3609cd64421a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bc63b1b57408d2328a7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "00221595863a748d1c6f",
+    "url": "/static/js/2.98414ef3.chunk.js"
+  },
+  {
+    "revision": "ef860ad130681bed940bf7f00cb4b53d",
+    "url": "/static/js/2.98414ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bc63b1b57408d2328a7",
+    "url": "/static/js/main.581c2688.chunk.js"
+  },
+  {
+    "revision": "7c0daf1a10d54e211f692f3cd1ed813b",
+    "url": "/static/js/main.581c2688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48e4ab88977f8baf5bee",
+    "url": "/static/js/runtime-main.581dc473.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
